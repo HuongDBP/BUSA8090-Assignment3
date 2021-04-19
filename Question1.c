8ce7e79dd46bd39cf3b3e56d58f5a3f431f17e17
HuongDBP <80447754+HuongDBP@users.noreply.github.com>
2021-04-20 03:24:50 +0700

    Delete Question1.c

--- Question1.c
+++
@@ -1,3 +0,0 @@
-sort -k1 -n annotation.tab > sorted_annotation.tab
-sort -k1 -n expression.tab > sorted_expression.tab
-join -t$'\t' -1 1 -2 1 -o 1.1,2.2,1.2,2.3 sorted_expression.tab sorted_annotation.tab > join.tab
